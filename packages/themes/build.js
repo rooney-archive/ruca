b468d2371c0f5480d8f8e147fb7b7a904c640fb7
rooney <bch3454@gmail.com>
2024-06-06 17:52:09 +0900

    feat: esbuild 패키지로 분리

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,43 +1,6 @@
-import esbuild from "esbuild";
+import run from "@ruca/esbuild-config";
 import pkg from "./package.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch,
-  external,
-};
-
-Promise.all([
-  // esm
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  // cjs
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch((e) => {
-  console.error("Build failed :", e);
-  // 에러 발생시 프로세스 종료
-  process.exit(1);
+run({
+  pkg,
 });
